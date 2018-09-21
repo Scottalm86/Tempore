8d9888243a16a4873fa82dd737be8fa95751f388
scott alm <scottalm83@gmail.com>
2018-09-21 22:31:24 +0200

    A Commit

--- Tempore.Spring.app/src/main/java/tempore/personRegister/PersonController.java
+++ Tempore.Spring.app/src/main/java/tempore/personRegister/PersonController.java
@@ -1,5 +1,14 @@
 package tempore.personRegister;
 
 public class PersonController {
-
+	
+	public String Person;
+	public String PersonSchool;
+	public String PersonAdress;
+	public long PersonId;
+	
+	private PersonController() {
+		
+	}
+	
 }
