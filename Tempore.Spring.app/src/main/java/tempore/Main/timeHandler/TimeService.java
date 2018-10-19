66823cffba6846a4b8ae3480858c0e3716b6baab
Scott Alm <scott.alm@elev.ga.klaragymnasium.se>
2018-10-19 23:30:39 +0200

    First commit from Edmill's Computer

--- Tempore.Spring.app/src/main/java/tempore/Main/timeHandler/TimeService.java
+++ Tempore.Spring.app/src/main/java/tempore/Main/timeHandler/TimeService.java
@@ -9,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class TimeService {
 	
-	private final static Object SL_URL = "https://api.sl.se/api2/TravelplannerV3/trip.json?key=aa704a5c893c433f9c0a38c00371a300";
+	private final static Object SL_URL = 
+			"https://api.sl.se/api2/TravelplannerV3/trip.json?key=aa704a5c893c433f9c0a38c00371a300";
 	private final static String SL_LANG = "&sv";
 	public String Orgin_ID_Latitude ="&originId=3814"/*+ longitude and latitude class */;
 	//public String Orgin_ID_Longitud ="&originCoordLong="/*+ longitude and latitude class */;
@@ -19,7 +20,8 @@ public class TimeService {
 	
 	
 	public Object  getResponse() {
-		ResponseEntity<Object> response = restTemplate.getForEntity(SL_URL+SL_LANG+Orgin_ID_Latitude+Destination_ID_Parmeter/*Time_Parmeter*/,
+		ResponseEntity<Object> response = restTemplate.
+				getForEntity(SL_URL+SL_LANG+Orgin_ID_Latitude+Destination_ID_Parmeter/*Time_Parmeter*/,
 				Object.class);
 		return response;
 	}
